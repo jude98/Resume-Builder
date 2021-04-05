84fdbd5bc29152a478dcde04b3b1b543a47056cd
Judith <judithgeorgejoseph123@gmail.com>
2021-04-06 01:42:32 +0530

    Hash Routing

--- resume-builder-app/src/App.js
+++ resume-builder-app/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Nav from "./Components/Nav";
 import Home from "./Components/Home";
 import CreateResume from "./Components/CreateResume";
